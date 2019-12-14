761b23d6fb2cd7ed7dbdba391e959262bb8e3359
khoingx <55979401+khoingx@users.noreply.github.com>
2019-12-14 13:44:39 +1100

    switch role

--- index.js
+++ index.js
@@ -6,23 +6,60 @@ inquirer
     .prompt([
         {
             type: "input",
-            message: "What is your role manager, engineer or intern ?",
+            message: "What is your name?",
             name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your ID?",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "What is your role manager, engineer or intern ?",
+            name: "role"
         }
     ])
     .then(answers => {
-        if (answers.name === 'manager') {
-            console.log('manager');
+        // console.log(answers);
+        switch(answers.role) {
+            case 'manager':
+            inquirer
+            .prompt({
+                type: "input",
+                message: "What is your office number?",
+                name: "officeNumber"
+            });
+            break;
+            case 'engineer':
+            inquirer
+            .prompt({
+                type: "input",
+                message: "What is your github username?",
+                name: "github"
+            });
+            break;
+            case 'intern':
+            inquirer
+            .prompt({
+                type: "input",
+                message: "What is the school you are coming from?",
+                name: "school"
+            })
         }
-        else if (answers.name ==='engineer') {
-            console.log('engineer');
-        }
-        else if (answers.name ==='intern') {
-            console.log('intern');
-        }
-        else (
-            console.log("you do not enter valid data")
-        )
+
+        // if (answers.role === 'manager') {
+        //     console.log('manager');
+        // }
+        // else if (answers.role ==='engineer') {
+        //     console.log('engineer');
+        // }
+        // else if (answers.role ==='intern') {
+        //     console.log('intern');
+        // }
+        // else (
+        //     console.log("you do not enter valid data")
+        // )
     })
 
     
